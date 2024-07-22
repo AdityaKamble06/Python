5f5f2404b5ecb39454ba31e8b0b23983d9cfcd30
Aditya Kamble <164652674+AdityaKamble06@users.noreply.github.com>
2024-07-22 12:47:53 +0530

    Create Python Program to Add Two Matrices.py

---
+++ Python Program to Add Two Matrices.py
@@ -0,0 +1,28 @@
+#Python to add two matrices using nested loop
+
+x = [[12,7,3],
+     [ 4,5,6],
+     [ 7,8,9]]
+
+y = [[5,8,1],
+    [6,7,3],
+    [4,5,9]]
+
+result = [[0,0,0],
+          [0,0,0],
+          [0,0,0]]
+
+#iterate through rows
+for i in range(len(x)):
+    #iterate through columns
+    for j in range (len(x[0])):
+        result[i][j] = x[i][j] + y[i][j]
+
+for r in result:
+    print(r)
+
+
+#Output
+[17, 15, 4]
+[10, 12, 9]
+[11, 13, 18]
