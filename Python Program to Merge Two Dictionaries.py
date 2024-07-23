1914c79e054be3ed565d92e2a142009abca7e890
Aditya Kamble <164652674+AdityaKamble06@users.noreply.github.com>
2024-07-23 21:46:04 +0530

    Update Python Program to Merge Two Dictionaries.py

--- Python Program to Merge Two Dictionaries.py
+++ Python Program to Merge Two Dictionaries.py
@@ -17,3 +17,16 @@ print({**dict_1, **dict_2})
 
 #Output
 # {1: 'a', 2: 'c', 4: 'd'}
+
+#Example 3: Using copy() and update()
+
+dict_1 = {1: 'a', 2: 'b'}
+dict_2 = {2: 'c', 4: 'd'}
+
+dict_3 = dict_2.copy()
+dict_3.update(dict_1)
+
+print(dict_3)
+
+#Output
+# {2: 'b', 4: 'd', 1: 'a'}
