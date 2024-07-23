d19818c836568d50fe950f517f87f6d308d348fd
Aditya Kamble <164652674+AdityaKamble06@users.noreply.github.com>
2024-07-23 21:44:50 +0530

    Create Python Program to Merge Two Dictionaries.py

---
+++ Python Program to Merge Two Dictionaries.py
@@ -0,0 +1,19 @@
+# Example 1: Using the | Operator
+
+dict_1 = {1:'a', 2:'b'}
+dict_2 = {2:'c', 4:'d'}
+
+print(dict_1 | dict_2)
+
+#Output
+# {1: 'a', 2: 'c', 4: 'd'}
+
+# Example 2: Using the ** Operator
+
+dict_1 = {1:'a', 2:'b'}
+dict_2 = {2:'c', 4:'d'}
+
+print({**dict_1, **dict_2})
+
+#Output
+# {1: 'a', 2: 'c', 4: 'd'}
