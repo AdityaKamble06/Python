1b9833daf41d82adab235913a51fced843c3ce1c
Aditya Kamble <164652674+AdityaKamble06@users.noreply.github.com>
2024-07-25 21:31:53 +0530

    Create Python Program to Create a Long Multiline String.py

---
+++ Python Program to Create a Long Multiline String.py
@@ -0,0 +1,25 @@
+# Example 1: Using triple quotes
+
+my_string = '''This only way to
+learn to program is
+by writing code.'''
+
+print(my_string)
+
+# Output
+# This only way to
+# learn to program is
+# by writing code.
+
+# Example 2: Using parentheses and a single/double quotes
+
+my_string = ("The only way to \n"
+             "learn to program is \n"
+             "by writing code.")
+
+print(my_string)
+
+# Output
+# This only way to
+# learn to program is
+# by writing code.
