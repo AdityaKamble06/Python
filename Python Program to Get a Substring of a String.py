bda21a5004fcf43db6e18393e11d5f316f0bfcb1
Aditya Kamble <164652674+AdityaKamble06@users.noreply.github.com>
2024-07-25 19:42:21 +0530

    Create Python Program to Get a Substring of a String.py

---
+++ Python Program to Get a Substring of a String.py
@@ -0,0 +1,15 @@
+my_string = "I love python."
+
+# prints "love"
+print(my_string[2:6])
+
+# prints "love python."
+print(my_string[2:])
+
+# prints "I love python"
+print(my_string[:-1])
+
+#Output
+# love
+# love python.
+# I love python
